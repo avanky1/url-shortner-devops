40742452c2ff1726f22179dcaf48e42f0c61f4ab
avanky1 <onlyfreematerials@gmail.com>
2026-06-24 20:46:05 +0530

    node-app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Dockerized Node.js API"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
